6820a770ee82ba97fbc9550a4290ba59a3073cd5
naleite <naleite.nuaa@gmail.com>
2014-10-12 15:01:46 +0200

    Merge branch 'master' into test_na

--- src/test/java/system/TestSequenceDiagram.java
+++ src/test/java/system/TestSequenceDiagram.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import simpleGame.core.Board;
@@ -15,11 +14,8 @@ import simpleGame.core.Pawn;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.mockingDetails;
 
 /**
  * Tests for the sequence diagrams.
@@ -146,5 +142,76 @@ public class TestSequenceDiagram extends Game {
         assertEquals(3,max);
     }
 
+// Belows are Josian's tests
+
+
+    /**
+     * @see simpleGame.core.Game#isGameOver()
+     */
+    @Test
+    public void TestIsGameOver() throws Exception
+    {
+
+        Game g1 = new Game();
+
+        //utilisation de la reflection pour set le board a un mockitos
+        Field field = g1.getClass().getDeclaredField("board");//recupere l'attibut de Game
+        field.setAccessible(true);//on le rend accessible
+        Object b = mock(Board.class);//instancie un mockito
+        field.set(g1,(Board)b); //
+
+        when(((Board) b).numberOfPawns()).thenReturn(2);
+        when(((Board) b).maxGold()).thenReturn(3);
+        //tester appel a numberofpawns
+        // puis maxgold()
+
+        org.junit.Assert.assertTrue(g1.isGameOver());
+
+        //dans l'ordre il va executer numberofpawns() puis mawGold
+        InOrder mocksWithOrder = inOrder(b);
+
+        mocksWithOrder.verify((Board) b).numberOfPawns();
+        mocksWithOrder.verify((Board) b).maxGold();
+
+
+    }
+
+
+
+    /**
+     * @see simpleGame.core.Game
+     */
+    @Test
+    public void TestMaxGold()
+    {
+        Board b = new Board(0,4,4,2,2);
+        b.removeAllPawns();
+
+        Pawn p1 = mock(Pawn.class);
+        Pawn p2 = mock(Pawn.class);
+
+        //pour etre sur qu'on va bien ajouter
+        //deux pawns(deux position differentes)
+
+        when(p1.getX()).thenReturn(3);
+        when(p1.getY()).thenReturn(3);
+        when(p2.getX()).thenReturn(2);
+        when(p2.getY()).thenReturn(2);
+
+
+        //on ajoute les pawn au board
+        b.addPawn(p1);
+        b.addPawn(p2);
+
+        when(p1.getGold()).thenReturn(1);
+        when(p2.getGold()).thenReturn(3);
+
+        org.junit.Assert.assertTrue(b.maxGold() == 3);
+
+        //dans l'ordre il va executer p1.getGold, puis p2.getGold()
+        InOrder mocksWithOrder = inOrder(p1, p2);
+        mocksWithOrder.verify(p1).getGold();
+        mocksWithOrder.verify(p2).getGold();
+    }
 
 }
