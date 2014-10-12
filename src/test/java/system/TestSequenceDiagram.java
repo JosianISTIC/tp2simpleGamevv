d1b584390084d981c966f897d9e5f955c73bad9b
josian <marinier.josian@yahoo.fr>
2014-10-12 14:34:37 +0200

    add testSequenceDiagram 2.0

--- src/test/java/system/TestSequenceDiagram.java
+++ src/test/java/system/TestSequenceDiagram.java
@@ -2,6 +2,7 @@ package system;
 import junit.framework.Assert;
 import org.junit.Test;
 import org.mockito.InOrder;
+import org.mockito.stubbing.OngoingStubbing;
 import simpleGame.core.Board;
 
 import static org.mockito.Mockito.inOrder;
@@ -9,6 +10,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import simpleGame.core.*;
+
+import java.lang.reflect.Field;
+
 /**
  * Created by josian on 12/10/14.
  */
@@ -19,20 +23,33 @@ public class TestSequenceDiagram{
      * @see simpleGame.core.Game#isGameOver()
      */
     @Test
-    public void TestIsGameOver()
+    public void TestIsGameOver() throws Exception
     {
 
-        Game g = new Game();
+        Game g1 = new Game();
+        //utilisation de la reflection pour set le board a un mockitos
+        Field field = g1.getClass().getDeclaredField("board");
+        field.setAccessible(true);
+        Object b = mock(Board.class);//instancie un mockito
+        field.set(g1,(Board)b);
 
-        /**Game g1 = mock(Game.class);
+        when(((Board) b).numberOfPawns()).thenReturn(2);
+        when(((Board) b).maxGold()).thenReturn(3);
+        //tester appel a numberofpawns
+        // puis maxgold()
 
-        when(g1.).thenReturn(1);
-        when(g2.getGold()).thenReturn(2);
+        org.junit.Assert.assertTrue(g1.isGameOver());
 
-        InOrder mocksWithOrder = inOrder(p1, p2);
-        mocksWithOrder.verify(p1).getGold();
-        mocksWithOrder.verify(p2).getGold();**/
+        //dans l'ordre il va executer p1.getGold, puis p2.getGold()
+        InOrder mocksWithOrder = inOrder(b);
+
+        mocksWithOrder.verify((Board) b).numberOfPawns();
+        mocksWithOrder.verify((Board) b).maxGold();
+
+
     }
+
+
 
     /**
      * @see simpleGame.core.Game
